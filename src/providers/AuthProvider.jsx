9e4c77a88be90756f9e104b4171c515ea295fe3d
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-05-06 07:47:51 +0600

    First Commit

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -9,37 +10,50 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    // Function to create a new user
     const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    // Function to sign in existing user
     const signIn = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    // Function to log out user
     const logOut = () => {
         setLoading(true);
         return signOut(auth);
     }
 
     useEffect(() => {
+        // Listen for authentication state changes
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('current user', currentUser);
             setLoading(false);
+            if (currentUser) {
+                // If user is logged in, get JWT token
+                const loggedUser = { email: currentUser.email };
+                axios.post("http://localhost:5000/jwt", loggedUser, { withCredentials: true })
+                    .then(res => {
+                        console.log("token Response", res.data)
+                    })
+                    .catch(error => {
+                        console.error("Error fetching JWT token:", error);
+                    });
+            }
         });
-        return () => {
-            return unsubscribe();
-        }
-    }, [])
+        // Unsubscribe from the listener when component unmounts
+        return unsubscribe;
+    }, []);
 
     const authInfo = {
         user,
         loading,
-        createUser, 
-        signIn, 
+        createUser,
+        signIn,
         logOut
     }
 
